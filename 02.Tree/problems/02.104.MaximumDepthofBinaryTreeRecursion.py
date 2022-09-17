f4750959c366a588a0e0140060a2d9d44896bb3f
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2022-09-17 10:44:11 +0200

    using recursion to get the max depth which is the lenght of the path from the root to node

---
+++ 02.Tree/problems/02.104.MaximumDepthofBinaryTreeRecursion.py
@@ -0,0 +1,17 @@
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# RECURSIVE DFS
+class Solution:
+    def maxDepth(self, root:TreeNode):
+        if not root:
+            return  0
+
+        return 1 + max(
+            self.maxDepth(root.left) ,
+            self.maxDepth(root.right)
+        )
