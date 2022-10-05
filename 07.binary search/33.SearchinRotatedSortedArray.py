ec18943f4110ee6d46d0ecf6b873ed8026706aba
ishiekh-dev <ishiekh.dev.v1@gmail.com>
2022-10-05 20:33:04 +0200

    solve this problem was not easy , as has to group sorted in different way -> BS O(n) is log(n) which indicate number of operations

---
+++ 07.binary search/33.SearchinRotatedSortedArray.py
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+binary search keep divide the array  by  2 -> which lead n/2**k -> k = log(n) number of operation
+"""
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        # loop two ponter means n/2 elements will be searched
+        while l <= r:
+            # get middle element
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion -> which could be sorted larger than right one
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
